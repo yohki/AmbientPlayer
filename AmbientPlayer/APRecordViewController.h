c75f41b74e7fca67c7c5a7eb18d15360005ec84a
Masato setoyama <setoyama60jp@gmail.com>
2012-09-08 17:24:03 +0900

    added  empty recording view

---
+++ AmbientPlayer/APRecordViewController.h
@@ -0,0 +1,13 @@
+//
+//  APRecordViewController.h
+//  AmbientPlayer
+//
+//  Created by 瀬戸山 雅人 on 2012/09/08.
+//  Copyright (c) 2012年 InteractionPlus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APRecordViewController : UIViewController
+
+@end
---
+++ AmbientPlayer/APRecordViewController.m
@@ -0,0 +1,43 @@
+//
+//  APRecordViewController.m
+//  AmbientPlayer
+//
+//  Created by 瀬戸山 雅人 on 2012/09/08.
+//  Copyright (c) 2012年 InteractionPlus. All rights reserved.
+//
+
+#import "APRecordViewController.h"
+
+@interface APRecordViewController ()
+
+@end
+
+@implementation APRecordViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
